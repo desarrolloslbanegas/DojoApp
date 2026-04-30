d432a03500b490c8fb224bcecee40e59eab9e660
Lucas Matias Banegas <desarrolloslbanegas@gmail.com>
2026-04-30 16:31:05 -0300

    Configuración de variables de entorno

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,39 @@
 const mysql = require('mysql2');
 
+//DEVELOPMENT LOCAL
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '', 
+//   database: 'sistema_kiosco_panaderia'
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error conectando a la base: ' + err.stack);
+//     return;
+//   }
+//   console.log('Conectado a MySQL local con el ID ' + connection.threadId);
+// });
+
+// module.exports = connection;
+
+
+//PRODUCTION SERVER
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
-  database: 'sistema_kiosco_panaderia'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error conectando a la base: ' + err.stack);
+    console.error('Error conectando a la base:', err);
     return;
   }
-  console.log('Conectado a MySQL local con el ID ' + connection.threadId);
+  console.log('Conectado a MySQL con ID ' + connection.threadId);
 });
 
 module.exports = connection;
